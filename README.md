6534177b4d825d8b83d3eb1f1f8b70e8f21352bd
siery <siery@comic.com>
2022-01-11 10:59:35 +0100

    Accept screen parameter
    And update the README.md

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # oled-saver
-Basic scripts that will dimm the DP-4 screen after 5 minutes using xrandr and
-xdotool.
+Basic scripts that will dim the specified screen each n minutes using the Linux xrandr and xdotool.
--- oled-saver.rb
+++ oled-saver.rb
@@ -1,30 +1,54 @@
 module OLED_SAVER
   private
+  @argc = 0
+
+  def self.argc argc = @argc
+    @argc = argc
+  end
+
+  # This works as we only have two arguments to specify:
+  # [-s screen] [mints]
+  def self.user_screen
+    screen = false
+    if ARGV[argc] == '-s' and ARGV.size >= 2
+      tmp_screen = ARGV[1]
+      if `xrandr | grep -o '#{tmp_screen} connected'`.empty?
+        warn "Specified screen is not connected (running defaults)"
+      else
+        argc 2
+        screen = tmp_screen
+      end
+    end
+    screen
+  end
+
   def self.user_time
-    time = ARGV[0].to_i
+    time = ARGV[argc].to_f
     time = nil if time == 0
     time
   end
 
+  SCREEN = self.user_screen || 'DP-4'
   MIN = self.user_time || 5
   SEC = MIN * 60
   @dimmed = false
-  puts "I will check on your mouse status each #{MIN} mints"
+  puts "Targeted screen: #{SCREEN}"
+  puts "Delta time: #{MIN} mints"
   
   public
   def self.run
-    trap('INT') do puts ''; exit 0 end
+    trap 'INT' do puts ''; exit 0 end
     xp = yp = 0
 
     while true do
       x, y = `xdotool getmouselocation --shell`[/X=(\d+)\nY=(\d+)/].lines.map do |v| v[/.=(\d+)/, 1].to_i end
       if xp == x and yp == y
-        `xrandr --output DP-4 --brightness 0.0`
+        `xrandr --output #{SCREEN} --brightness 0.0`
         @dimmed = true
       else
-        (@dimmed = false; `xrandr --output DP-4 --brightness 1.0`) if @dimmed == true
+        (@dimmed = false; `xrandr --output #{SCREEN} --brightness 1.0`) if @dimmed == true
         xp, yp = x, y
-        sleep(SEC)
+        sleep SEC
       end
     end
   end
